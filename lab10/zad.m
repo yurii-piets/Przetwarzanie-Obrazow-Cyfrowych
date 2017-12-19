1343fd0bb283205e26ac4d019f64367c6479450a
Yurii Piets <yurii.piets@gmail.com>
2017-12-19 13:13:39 +0100

    implemeted fitrst part of task 1

--- lab10/zad.m
+++ lab10/zad.m
@@ -1,56 +1,65 @@
-clearvars; close all; clc;
+close all; clearvars; clc;
 
-BRD = 4;
 image = imread('knee.png');
-image = im2double(image);
+image = double(image);
 
-%[x y] = floor(ginput(1));
+x = 280;
+y = 320;
 
-x = 330;
-y = 205;
-
-[n, m] = size(image);
+[n m] = size(image);
 
 visited = zeros(n, m);
 segmented = zeros(n, m);
-stack = zeros(10000,2);
+stack = zeros(10000, 2);
 iStack = 1;
 
-stack(iStack, :) = [x y];
+stack(iStack, :) = [x, y];
 visited(x, y) = 1;
-segmented(x, y) = image(x,y);
+segmented(x, y) = image(x, y);
 
-while iStack ~= 0
+BRD = 4;
+
+while iStack > 0
     x = stack(iStack, 1);
     y = stack(iStack, 2);
     iStack = iStack - 1;
     
-    if(~(x - 1 <= 0 || x + 1 > n || y - 1 <= 0 || y + 1 > m))
-       segmented(x, y) = image(x,y);
-       brightness = image(x,y); 
-       
-        for i=-1:1
-            for j=-1:1
-                if( ~( i == 0 && j == 0) )
-                    brightnessNeighbour = image(x+i, y+j);
-                    diff = abs(brightness-brightnessNeighbour);
-                    
-                    if (visited(x+i,y+j) == 0 && diff < BRD)
-                        iStack = iStack + 1;
-                        stack(iStack, :) = [x y];
-                    end
-                    
-                    visited(x+i, y+j) = 1;
-                end
-            end
-        end
-        
-        visited(x, y) = 1;
+    if x <= 1 || x >= n
+        continue;
     end;
+    
+    if y <= 1 || y >= m
+        continue;
+    end;
+    
+    brightness = image(x, y);
+    
+    for i = -1:1
+        for j = -1:1
+            if i == 0 && j == 0
+                continue;
+            end;
+            
+            if visited(x+i, y+j) == 1
+                continue;
+            end;
+            
+            visited(x+i, y+j) = 1;
+            
+            brightnessLocal = image(x+i, y+j);
+            diff = abs(brightness - brightnessLocal);
+            
+            if diff < BRD
+                segmented(x+i, y+j) = image(x+i, y+j);
+                iStack = iStack + 1;
+                stack(iStack, 1) = x+i;
+                stack(iStack, 2) = y+j;
+            end
+        end;
+    end
     
 end;
 
-figure;
 subplot(1,3,1); imshow(image, []); title('oryginal');
-subplot(1,3,2); imshow(segmented, []); title('segmented');
-subplot(1,3,3); imshow(visited, []); title('visited');
+subplot(1,3,2); imshow(visited, []); title('visited');
+subplot(1,3,3); imshow(segmented, []); title('segmented');
